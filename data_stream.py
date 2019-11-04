b55092c568badc8f371d069f8e484420ebe2817f
Theoneg <52198010+Theoneg@users.noreply.github.com>
2019-11-04 11:55:46 +0100

    Updated the file for submission.

--- data_stream.py
+++ data_stream.py
@@ -6,80 +6,93 @@ import pyspark.sql.functions as psf
 from dateutil.parser import parse as parse_date
 
 
-
+# TODO Create a schema for incoming resources
+# schema
 schema = StructType([
-    StructField("offense_date", StringType(), True),
-    StructField("address_type", StringType(), True),
+    StructField("crime_id", IntegerType(), True),
+    StructField("original_crime_type_name", StringType(), True),
+    StructField("report_date", TimestampType(), True),
+    StructField("call_date", TimestampType(), True),
+    StructField("offense_date", TimestampType(), True),
+    StructField("call_time", TimestampType(), True),
+    StructField("call_date_time", TimestampType(), True),
     StructField("disposition", StringType(), True),
-    StructField("agency_id", StringType(), True),
-    StructField("common_location", StringType(), True),
+    StructField("address", StringType(), True),
     StructField("city", StringType(), True),
     StructField("state", StringType(), True),
-    StructField("call_date", StringType(), True),
-    StructField("call_date_time", StringType(), True),
-    StructField("report_date", StringType(), True),
-    StructField("crime_id", StringType(), True),
-    StructField("call_time", StringType(), True),
-    StructField("address", StringType(), True),
-    StructField("original_crime_type_name", StringType(), True)
+    StructField("agency_id", IntegerType(), True),
+    StructField("address_type", StringType(), True),
+    StructField("common_location", StringType(), True)
 ])
 
-
+# TODO create a spark udf to convert time to YYYYmmDDhh format
 @psf.udf(StringType())
 def udf_convert_time(timestamp):
-    d = parse_date(timestamp)
-    return str(d.strftime('%y%m%d%H'))
-    
+    date_time = parse_date(timestamp)
+    return str(date_time.strftime('%y%y%d%H'))
+
+
 def run_spark_job(spark):
 
+    # TODO Create Spark Configuration
+    # Create Spark configurations with max offset of 200 per trigger
+    # set up correct bootstrap server and port
+    # df = spark ...
     df = spark \
-    .readStream \
-    .format("kafka") \
-    .option("kafka.bootstrap.servers", "localhost:9092") \
-    .option("subscribe", "service-calls") \
-    .option("startingOffsets", "earliest") \
-    .option("maxOffsetPerTrigger", "200") \
-    .load()
+        .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "localhost:9020") \
+        .option("subscribe", "service-calls") \
+        .option("startingOffserts", "earliest") \
+        .option("maxRatePerPartition", 100) \
+        .option("maxOffsetPerTrigger", 10) \
+        .load()
+    
 
-     
+    # Show schema for the incoming resources for checks
     df.printSchema()
-    
-    kafka_df = df.selectExpr("CAST(value AS STRING)")
-    
-    
+
+    # TODO extract the correct column from the kafka input resources
+    # Take only value and convert it to String
+    kafka_df = df.selectExpr("CAST (value AS STRING)")
+
     service_table = kafka_df\
         .select(psf.from_json(psf.col('value'), schema).alias("SERVICE_CALLS"))\
         .select("SERVICE_CALLS.*")
 
-    # service_table.printSchema()
-
-    distinct_table = service_table \
+    distinct_table = service_table\
         .select(psf.col('crime_id'),
                 psf.col('original_crime_type_name'),
                 psf.to_timestamp(psf.col('call_date_time')).alias('call_datetime'),
                 psf.col('address'),
                 psf.col('disposition'))
 
-    # distinct_table.printSchema()
+    # TODO get different types of original_crime_type_name in 60 minutes interval
+    counts_df = distinct_table\
+        .withWatermark("original_crime_type_name", "60 minutes")\
+        .groupBy(psf.window(distinct_table.call_datetime,
+                            "10 minutes", 
+                            "10 minutes"),
+                 distinct_table.original_crime_type_name)\
+                 .count()
+
+    # TODO use udf to convert timestamp to right format on a call_date_time column
+    converted_df = counts_df\
+        .withColumn("call_datetime", udf_convert_time(counts_df.call_datetime))
 
 
-    
-    counts_df = distinct_table \
-        .withWatermark("call_datetime", "60 minutes") \
-        .groupBy(
-            psf.window(distinct_table.call_datetime, "10 minutes", "5 minutes"),
-            distinct_table.original_crime_type_name
-            ).count()
+    # TODO apply aggregations using windows function to see how many calls occurred in 2 day span
+    calls_per_2_days = converted_df \
+            .groupBy(psf.window(converted_df.call_date_time, "2 day"))\
+            .agg(psf.count("crime_id").alias("calls_per_2_day"))\
+            .select("calls_per_2_day")
 
-    # counts_df.printSchema()
-
-
-    query = counts_df \
-        .writeStream \
-        .outputMode('complete') \
-        .format('console') \
+    # TODO write output stream
+    query = service_table\
+        .writeStream\
+        .outputMode("append")\
+        .format("console")\
         .start()
-     
 
     # TODO attach a ProgressReporter
     query.awaitTermination()
@@ -89,15 +102,14 @@ if __name__ == "__main__":
     logger = logging.getLogger(__name__)
 
     # TODO Create Spark in Local mode
-    spark = SparkSession \
-        .builder \
+    spark = SparkSession.builder \
         .master("local") \
-        .appName('SF Crime') \
+        .appName("SF Crime Statitics") \
         .getOrCreate()
-    
-    spark.sparkContext.getConf().getAll()
+
     logger.info("Spark started")
-    
+
     run_spark_job(spark)
 
     spark.stop()
+
